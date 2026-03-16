0461b3f92fb0c95bb1053026079512d7b7c8ccad
LieTH <lietha.arrah12@gmail.com>
2026-03-16 19:48:21 +0200

    Add TypeScript types to request handlers

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import images from './routes/images';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response): void => {
   res.send('Image API working');
 });
 
 app.use('/api/images', images);
 
-app.use((err: Error, req: Request, res: Response) => {
-  console.error(err.stack);
-  res.status(500).send('Internal server error');
-});
+app.use(
+  (err: Error, req: Request, res: Response): void => {
+    console.error(err.stack);
+    res.status(500).send('Internal server error');
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
